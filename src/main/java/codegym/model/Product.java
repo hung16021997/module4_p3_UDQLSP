2f77444fa9339fa62c024dea35ded838c8baeeab
nguyenmanhhung16021997 <nguyenmanhhung16021997@gmail.com>
2020-08-19 21:09:09 +0700

    done

---
+++ src/main/java/codegym/model/Product.java
@@ -0,0 +1,50 @@
+package codegym.model;
+
+public class Product {
+    private int id;
+    private String name;
+    private long price;
+    private int amount;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, long price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
